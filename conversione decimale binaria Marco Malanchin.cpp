de52dd78a98c680439c0d904a581b4c5849757aa
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2020-11-09 17:04:11 +0100

    Create conversione decimale binaria Marco Malanchin.cpp

---
+++ conversione decimale binaria Marco Malanchin.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>    
+#include<stdlib.h>  
+int main(){  
+int a[10],n,i;    
+system ("cls");  
+printf("metti il numero da convertire ");    
+scanf("%d",&n);    
+for(i=0;n>0;i++)    
+{    
+a[i]=n%2;    
+n=n/2;    
+}    
+printf("\nIl numero binario e''=");    
+for(i=i-1;i>=0;i--)    
+{    
+
+}    
+return 0;  
+}  
